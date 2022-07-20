f5444acb97b758217939bff4e8c06de8766f2951
Aryan Karan <aryankaran28022004@gmail.com>
2022-07-20 23:30:08 +0530

    nuke Gapps nd test

--- config/common.mk
+++ config/common.mk
@@ -253,11 +253,4 @@ $(call inherit-product, vendor/evolution/config/textclassifier.mk)
 # Inherit from themes config
 $(call inherit-product, vendor/evolution/config/themes.mk)
 
-# Inherit from GMS product config
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
-
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
